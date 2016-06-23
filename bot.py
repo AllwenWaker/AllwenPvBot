7ac4b85b756183db2caa997cadf2a357fd9ee58e
allwen <ArianGhasempour40@Gmail.com>
2016-06-23 15:29:43 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,15 +10,15 @@ import json
 import time
 from pprint import pprint
 
-bot = telebot.TeleBot("180026910:AAGdBKiObABjIQy-yOD7Ci1Dit9qW6aDqfM")
+bot = telebot.TeleBot("Your Token")
 
 @bot.message_handler(commands=['feedback'])
 def Hello(m):
     str = m.text
     txt = str.replace("/feedback", "")
     idA, cid = m.chat.id, m.chat.id
-    bot.send_message('-100576894', "*New massage* : \n\n ```{}``` \n\n _user id_ : *{}*".format(txt,idA), parse_mode="Markdown")
-    bot.reply_to(m, "*Your massage has send to allwen !*", parse_mode="Markdown")
+    bot.send_message('-100576894', "*° You Have New Message My Admin °*  : \n\n ```{}``` \n\n _Sender Id_ : *{}*".format(txt,idA), parse_mode="Markdown")
+    bot.reply_to(m, "*Your Message Recived To Allwen :)*", parse_mode="Markdown")
 
 @bot.message_handler(commands=['j'])
 def j(m):
@@ -27,16 +27,16 @@ def j(m):
         idA, cid = m.chat.id, m.chat.id
         for line in datafile:
             if str(tmt) not in datafile:
-                bot.send_message(cid, "Just for admin", parse_mode="Markdown")
+                bot.send_message(cid, "You're Nit Allwen !", parse_mode="Markdown")
                 return
         to_id = m.text.split()[1]
         txt = m.text.split()[2:]
         text = ' '.join(txt)
         from_id = m.from_user.first_name
-        bot.send_message(to_id, "{}Answer : \n ```{}```".format(from_id,text), parse_mode="Markdown")
+        bot.send_message(to_id, "{}Allwens Answer: \n ```{}```".format(from_id,text), parse_mode="Markdown")
 
 @bot.message_handler(commands=['start', 'help'])
 def welcome(message):
-    bot.reply_to(message, "Hello welcome to *Allwen* Support \n\n [create Your bot](https://telegram.me/antispam_api_bot)\n\n\n send pm : /feedback [text]", parse_mode="Markdown")
+    bot.reply_to(message, "Hello welcome to *Allwen* Support \n\n [Made Your Bot!](https://telegram.me/MadeYourBots)\n\n\n Send Message : /feedback [text]", parse_mode="Markdown")
 
 bot.polling()
